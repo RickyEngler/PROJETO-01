b73f2cbf937ce89ae721e6b3e22f194cbc782ef3
Augusto Henrique Engler <144976023+RickyEngler@users.noreply.github.com>
2024-08-20 11:46:44 -0300

    commit test

---
+++ java-script/funcoes.js
@@ -0,0 +1,10 @@
+function logar () {
+    var login = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin01" && "admin01"){
+        alert('Sucesso');
+    }
+        
+}
+    
