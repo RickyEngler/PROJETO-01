2e53e37fd84677cc505eace649db0195e40116ce
Augusto Henrique Engler <144976023+RickyEngler@users.noreply.github.com>
2024-08-22 12:12:52 -0300

    Update 2.0.3
    **NOTES
    
    - POP-UP DE CADASTRO ADICIONADO
    - CÓDIGO FONTE ALTERADO E ATUALIZADO

--- java-script/login.js
+++ java-script/login.js
@@ -1,15 +1,13 @@
-document.getElementById('open-register').addEventListener('click', function(event) {
-    event.preventDefault(); // Impede o comportamento padrão do link
-    document.getElementById('register-popup').style.display = 'flex'; // Exibe o pop-up
+document.getElementById("openPopupBtn").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "flex";
 });
 
-document.getElementById('close-popup').addEventListener('click', function() {
-    document.getElementById('register-popup').style.display = 'none'; // Oculta o pop-up
+document.getElementById("closePopupBtn").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "none";
 });
 
-// Fechar o pop-up ao clicar fora do conteúdo
-window.addEventListener('click', function(event) {
-    if (event.target === document.getElementById('register-popup')) {
-        document.getElementById('register-popup').style.display = 'none';
+window.addEventListener("click", function(event) {
+    if (event.target == document.getElementById("popupForm")) {
+        document.getElementById("popupForm").style.display = "none";
     }
 });
