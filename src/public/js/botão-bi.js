f1053b53dc75dd180c26e295018c0d250abf6bc6
Augusto Henrique Engler <144976023+RickyEngler@users.noreply.github.com>
2025-03-27 19:55:53 -0300

    add-chatbox

--- src/public/js/botão-bi.js
+++ src/public/js/botão-bi.js
@@ -1,10 +1,15 @@
 function togglePainel(url) {
-    var frame = document.getElementById('painel-frame');
-    if (frame.style.display === 'block' && frame.src === url) {
-        frame.style.display = 'none';
-        frame.src = '';
+    var painelButton = event.target.closest('.painel-central-de-sup');
+    var existingIframe = painelButton.nextElementSibling && painelButton.nextElementSibling.tagName === 'IFRAME';
+    
+    if (existingIframe) {
+        painelButton.nextElementSibling.remove();
     } else {
-        frame.style.display = 'block';
-        frame.src = url;
+        var iframe = document.createElement('iframe');
+        iframe.src = url;
+        iframe.style.width = "80%";
+        iframe.style.height = "600px";
+        iframe.frameBorder = "0";
+        painelButton.insertAdjacentElement('afterend', iframe);
     }
 }
